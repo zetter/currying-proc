251dba21f6b01f982722b3904a7c3eda40638baf
Zetter <zetter@gmail.com>
2014-08-03 17:56:59 +0100

    Implementation for currying proc.

--- currying_proc.rb
+++ currying_proc.rb
@@ -1,2 +1,17 @@
 class CurryingProc
+  def initialize(*args, &block)
+    @args = args
+    @block = block
+  end
+
+  def call(*new_args)
+    args = @args + new_args
+    if args.length == @block.arity
+      @block.call(*args)
+    else
+      self.class.new(*args, &@block)
+    end
+  end
+
+  alias_method :[], :call
 end
